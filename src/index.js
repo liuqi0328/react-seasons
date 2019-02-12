c58e82a849a36fdbfaa0177654f5d57006e1da13
liuqi0328 <lqyterrible@gmail.com>
2019-02-12 15:34:29 -0500

    add componentDidMount/Update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+  // eslint-disable-next-line no-useless-constructor
+  constructor(props) {
+    super(props);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    // THIS IS THE ONLY TIME we do direct assignment to this.state
+    // Initialization
+    this.state = { lat: null, errorMessage: '' };
+  }
+
+  componentDidMount() {
+      // console.log('My component was rendered to the screen!');
+      window.navigator.geolocation.getCurrentPosition(
+        // we called setState! Never ever straightly assign value to this.state
+        // like this.state.lat = position.coords.latitude
+        position => this.setState({ lat: position.coords.latitude }),
+        err => this.setState({ errorMessage: err.message })
+    );
+  }
+
+  componentDidUpdate() {
+      console.log('My component was just updated - it rerendered!');
+  }
+
+  // We have to define render in every react component.
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+        return <div>Latitude: {this.state.lat}</div>
+    }
+
+    return <div>Loading...</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
